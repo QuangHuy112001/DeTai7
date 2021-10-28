ad1fc7969d22e04022f7120f0bd94184f1d90d45
chinhvu1031 <75462255+chinhvu1031@users.noreply.github.com>
2021-10-28 10:57:52 +0700

    .

--- Index.php
+++ Index.php
@@ -18,7 +18,7 @@
         <div class="row">
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid">
-                    <img src="img/logo.png" alt="wowFood" class="img-fluid">
+                    <img src="img/logo.png" alt="" class="img-fluid">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
---
+++ Login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form-Login</title>
+</head>
+<body>
+    <div class="container">
+        <form class="form-login">
+            <h1>Đăng nhập</h1>
+            <div class="form-text">
+                <label>Username</label>
+                <input type="text">
+            </div>
+
+            <div class="form-text">
+                <label>Password</label>
+                <input type="password">
+            </div>
+
+            <button>Đăng nhập</button>
+            <span>Bạn chưa có tài khoản? Đăng kí<a href="" >Tại đây</a></span>
+
+        </form>
+    </div>
+    
+</body>
+</html>
