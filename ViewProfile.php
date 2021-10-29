192d146cada04eb6c54230c73d30ea4714746868
chinhvu1031 <75462255+chinhvu1031@users.noreply.github.com>
2021-10-30 03:05:48 +0700

    .

--- ViewProfile.php
+++ ViewProfile.php
@@ -30,7 +30,7 @@ include 'header.php';
                         <h3>Quang Huy</h3>
                         <a href="#">Home</a>
                         <hr>
-                        <a href="#">Contact</a>
+                        <a href="Contact.php">Contact</a>
                         <hr>
                         <a href="#">Setting</a>
                     </div>
