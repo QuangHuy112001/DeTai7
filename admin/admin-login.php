e60e7778154d7c36b37f62ecbb17241a465ba4c4
QuangHuy112001 <91008548+QuangHuy112001@users.noreply.github.com>
2021-10-29 15:30:23 +0700

    .

---
+++ admin/admin-login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <title>Form-Login</title>
+</head>
+<body>
+    <div class="container">
+        <form class="form-login">
+            <h1>Login admin</h1>
+            <div class="form-text">
+                <label>Username</label>
+                <input type="text">
+            </div>
+
+            <div class="form-text">
+                <label>Password</label>
+                <input type="password">
+            </div>
+
+            <button><a href="user-list.php">Login</a></button>
+
+        </form>
+    </div>
+
+    <script>
+        const formLogin = document.querySelectorAll('.form-text input')
+        const formLabel = document.querySelectorAll('.form-text label')
+        for(let i=0;i<2;i++){
+            formLogin[i].addEventListener("mouseover",function(){
+                formLabel[i].classList.add("focus")
+            })
+            formLogin[i].addEventListener("mouseout",function(){
+                formLabel[i].classList.remove("focus")
+            })
+        }
+    </script>
+    
+</body>
+</html>
