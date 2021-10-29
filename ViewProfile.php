2eb610fe993f8ae74fbc8b657545f3fca1970c5b
chinhvu1031 <75462255+chinhvu1031@users.noreply.github.com>
2021-10-29 22:42:46 +0700

    thêm form profile

--- ViewProfile.php
+++ ViewProfile.php
@@ -1,8 +1,9 @@
 <?php
 include 'header.php';
 ?>
+<link rel="stylesheet" href="css/ViewProfile.css">
 <!--Logo-->
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
@@ -13,8 +14,6 @@ include 'header.php';
                     <ul class="navbar-nav ms-auto mb-2 mb-1g-0">
                         <nav class="navbar navbar-light ">
                             <form class="container-fluid justify-content-start">
-                                <button class="btn btn-outline-success me-2" type="button" href="#"><a href="Login.php" style="text-decoration: none; color:seagreen;">Login</a></button>
-                                <button class="btn btn-outline-success me-2" type="button" href="#"><a href="CreateAcc.php" style="text-decoration: none; color:seagreen;">Create Account</a></button>
                                 <button class="btn me-2" type="button" href="#"><a href="#" style="text-decoration: none; color:grey;">Logout</a></button>
                             </form>
                         </nav>
@@ -22,13 +21,87 @@ include 'header.php';
                 </div>
             </div>
         </nav>
+
+        <div class="col-md-4 mt-1">
+            <div class="card texr-center sidebar">
+                <div class="card-body">
+                    <img src="img/avatar-user.png" alt="" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                        <h3>Quang Huy</h3>
+                        <a href="#">Home</a>
+                        <hr>
+                        <a href="#">Contact</a>
+                        <hr>
+                        <a href="#">Setting</a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-8 mt-1">
+            <div class="card mb-3 content">
+                <h1 class="m-3 pt-3">About</h1>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h6>Full name</h6>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            Bùi Hữu Quang Huy
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h6>Sex</h6>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            Female
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h6>Job</h6>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            Student
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h6>Email</h6>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            huuhuy11112001@gmail.com
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h6>Phone</h6>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            0389770968
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h6>Address</h6>
+                        </div>
+                        <div class="col-md-9 text-secondary">
+                            street no.4, abc
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            
-        <div class="col-md-8"></div>
-    </div>
-</div>
+
+ 
