63585f537c7790da5953d51e28c063a3122e1641
chinhvu1031 <75462255+chinhvu1031@users.noreply.github.com>
2021-10-31 22:13:39 +0700

    .

--- footer.php
+++ footer.php
@@ -3,21 +3,10 @@
   <div class="container p-4 pb-0">
     <!-- Section: Social media -->
     <section class="mb-4">
-      <!-- Facebook -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f">Facebook</i></a>
-
       <!-- Twitter -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter">Twitter</i></a>
-
-      <!-- Google -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google">Google</i></a>
-
       <!-- Instagram -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram">Instagram</i></a>
-
-      <!-- Linkedin -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in">Linkedin</i></a>
-
       <!-- Github -->
       <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github">Github</i></a>
     </section>
