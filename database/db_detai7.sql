6630e20483824a5c97c762f709ef347609fc3486
TuanasTrung <91027912+TuanasTrung@users.noreply.github.com>
2021-11-01 22:15:03 +0700

    Update db_detai7.sql

--- database/db_detai7.sql
+++ database/db_detai7.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 30, 2021 lúc 04:29 PM
--- Phiên bản máy phục vụ: 10.4.21-MariaDB
--- Phiên bản PHP: 8.0.10
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2021 at 04:13 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `db_detai7`
+-- Database: `db_detai7`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_admin`
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `user_id` int(11) NOT NULL,
+  `user_one` int(10) UNSIGNED NOT NULL,
+  `user_two` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friend_request`
+--
+
+CREATE TABLE `friend_request` (
+  `user_id` int(11) NOT NULL,
+  `sender` int(10) UNSIGNED NOT NULL,
+  `receiver` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
 --
 
 CREATE TABLE `tbl_admin` (
@@ -35,7 +59,7 @@ CREATE TABLE `tbl_admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `tbl_admin`
+-- Dumping data for table `tbl_admin`
 --
 
 INSERT INTO `tbl_admin` (`admin_id`, `admin_fullname`, `admin_username`, `admin_password`) VALUES
@@ -46,10 +70,11 @@ INSERT INTO `tbl_admin` (`admin_id`, `admin_fullname`, `admin_username`, `admin_
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_profile`
+-- Table structure for table `tbl_profile`
 --
 
 CREATE TABLE `tbl_profile` (
+  `pro_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `fullname` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `sex` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `birthday` date DEFAULT NULL,
@@ -61,38 +86,59 @@ CREATE TABLE `tbl_profile` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `tbl_profile`
+-- Dumping data for table `tbl_profile`
 --
 
-INSERT INTO `tbl_profile` (`fullname`, `sex`, `birthday`, `job`, `email`, `phonenumber`, `address`, `user_id`) VALUES
-('Vũ Tiến Chinh', 'Nam', '2001-01-31', 'Sinh Viên', 'Chinhvu1031@gmail.com', '0389370877', 'Thái Bình', 1);
+INSERT INTO `tbl_profile` (`pro_id`, `fullname`, `sex`, `birthday`, `job`, `email`, `phonenumber`, `address`, `user_id`) VALUES
+('1', 'Vũ Tiến Chinh', 'Nam', '2001-01-31', 'Sinh Viên', 'Chinhvu1031@gmail.com', '0389370877', 'Thái Bình', 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_user`
+-- Table structure for table `tbl_user`
 --
 
 CREATE TABLE `tbl_user` (
   `user_id` int(10) UNSIGNED NOT NULL,
   `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
+  `user_password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_image` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `tbl_user`
+-- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`user_id`, `username`, `user_email`, `user_password`) VALUES
-(1, 'chinh', 'Chinhvu1031@gmail.com', '123456');
+INSERT INTO `tbl_user` (`user_id`, `username`, `user_email`, `user_password`, `user_image`) VALUES
+(1, 'chinh', 'Chinhvu1031@gmail.com', '123456', NULL),
+(2, 'trung', 'trung123@gmai.com', '$2y$10$59BKQzN4rgYXpoWKdbPHmu5hzVQJhHWw5J9uSov/sxk.MWL9sKTgC', '6.png'),
+(3, 'huy', 'huy123@gmail.com', '$2y$10$XQIcg6hi6ktJ07kdp3GiROXGWT8uEC0mzPm09tWZW1xrpGgq/DtjO', '7.png'),
+(4, 'hoàng anh', 'hanh123@gmail.com', '$2y$10$rkvsfqQiKA3p4uxoKT//8.L8hd28Qt0QHVQf0Z7JmWMwmb7QiENTy', '12.png'),
+(5, 'châm anh', 'chamanh123@gmail.com', '$2y$10$hoXxPDJ.9gcD6JXdJyn/FeN1N6SLHqFngER0tHKyZK/GDshBVcYlW', '9.png');
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `tbl_admin`
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_one` (`user_one`),
+  ADD KEY `user_two` (`user_two`);
+
+--
+-- Indexes for table `friend_request`
+--
+ALTER TABLE `friend_request`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `sender` (`sender`),
+  ADD KEY `receiver` (`receiver`);
+
+--
+-- Indexes for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
   ADD PRIMARY KEY (`admin_id`),
@@ -100,13 +146,14 @@ ALTER TABLE `tbl_admin`
   ADD UNIQUE KEY `admin_password` (`admin_password`);
 
 --
--- Chỉ mục cho bảng `tbl_profile`
+-- Indexes for table `tbl_profile`
 --
 ALTER TABLE `tbl_profile`
+  ADD PRIMARY KEY (`pro_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Chỉ mục cho bảng `tbl_user`
+-- Indexes for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`user_id`),
@@ -115,27 +162,53 @@ ALTER TABLE `tbl_user`
   ADD UNIQUE KEY `user_password` (`user_password`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `tbl_admin`
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `friend_request`
+--
+ALTER TABLE `friend_request`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
   MODIFY `admin_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT cho bảng `tbl_user`
+-- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- Constraints for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `tbl_profile`
+-- Constraints for table `friends`
+--
+ALTER TABLE `friends`
+  ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_one`) REFERENCES `tbl_user` (`user_id`),
+  ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`user_two`) REFERENCES `tbl_user` (`user_id`);
+
+--
+-- Constraints for table `friend_request`
+--
+ALTER TABLE `friend_request`
+  ADD CONSTRAINT `friendre_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `tbl_user` (`user_id`),
+  ADD CONSTRAINT `friendre_ibfk_2` FOREIGN KEY (`receiver`) REFERENCES `tbl_user` (`user_id`);
+
+--
+-- Constraints for table `tbl_profile`
 --
 ALTER TABLE `tbl_profile`
   ADD CONSTRAINT `tbl_profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`user_id`);
